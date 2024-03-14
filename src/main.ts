46e9c97065541849db678facb573ac2f3e801f17
Matthew Sweet <sweet@andrews.edu>
2024-03-14 00:39:37 -0500

    Changed PORT to env only

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.port || 80;
+  const port = process.env.port;
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
