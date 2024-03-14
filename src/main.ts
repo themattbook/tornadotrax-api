b0d55ac6b177848bf1c863c3af8c2e00fc38279c
Matthew Sweet <sweet@andrews.edu>
2024-03-14 00:43:05 -0500

    Updated env vars

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.port;
+  const port = process.env.PORT;
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
