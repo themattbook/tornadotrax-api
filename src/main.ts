c69d80ac48941012d55f4d187459844dcbf81f5b
Matthew Sweet <sweet@andrews.edu>
2024-03-14 00:17:14 -0500

    Added console output to determine port

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.port || 3000;
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
-  await app.listen(process.env.port || 3000);
+  await app.listen(port);
+  console.log(`Application is running on ${port}`);
 }
 bootstrap();
